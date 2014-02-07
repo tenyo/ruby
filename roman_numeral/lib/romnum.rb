37a02e244682833b9e6c6b3cfda921f56697c63a
tenyo <tenyo.grozev@yale.edu>
2014-02-06 23:19:04 -0500

    Update romnum.rb

--- roman_numeral/lib/romnum.rb
+++ roman_numeral/lib/romnum.rb
@@ -1,12 +1,12 @@
-
 class RomNum
 
   @@NUMERALS = { "I" => 1, "IV" => 4, "V" => 5, "IX" => 9, "X" => 10, 
                  "XL" => 40, "L" => 50, "XC" => 90, "C" => 100, 
                  "CD" => 400, "D" => 500, "CM" => 900, "M" => 1000 }
 
+  # does some basic validation, needs more
   def self.valid_roman?(numeral)
-    # does some basic validation, needs more
+    
 
     single_numerals = @@NUMERALS.keys.select { |x| x.size == 1 }
 
